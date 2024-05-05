33ad97da717ed4459fa469dc242330c69a0c73b7
Jonathan <jnbns42@gmail.com>
2024-05-05 13:03:01 +0100

    New types, env variable updates

--- sanity.config.ts
+++ sanity.config.ts
@@ -23,8 +23,8 @@ import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schema";
 
 export default defineConfig({
-  name: "project-name",
-  title: "Project Name",
+  name: "astro-demo",
+  title: "Astro Demo",
   projectId,
   dataset,
   plugins: [deskTool(), visionTool()],
